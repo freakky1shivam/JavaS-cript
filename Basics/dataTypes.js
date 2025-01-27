2749e73ff6c191a2b508a54fd531ac8f367ee724
Shivam Chaudhary <skc722768@gmail.com>
2025-01-27 14:36:28 +0000

    dataTypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3)  we are using nodejs, not browser
+// code readability  should be high
+let name = "Shivam"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+// *******Data Types*********
+// number => 2 to power 53
+// bigint
+// string => " "  ''
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined

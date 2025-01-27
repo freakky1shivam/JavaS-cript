273e930ea6bfc29a3ea84c20338155616a60d6e5
Shivam Chaudhary <skc722768@gmail.com>
2025-01-27 14:50:24 +0000

    conversionAndOperation

---
+++ Basics/conversionAndOperation.js
@@ -0,0 +1,19 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber  = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "22" => 22
+// "22abc" => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+// 0 => true, 0 => false
+// "" => false
+// "shivam" => true
+
+ 
